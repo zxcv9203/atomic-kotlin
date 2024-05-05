3b52fd681039ab5ae39a3a4d8fa9de087d83ee22
yongckim <zxcv9203@naver.com>
2024-05-05 17:14:44 +0900

    :sparkles: var와 val

---
+++ src/main/kotlin/chapter01/sector04/Vals.kt
@@ -0,0 +1,18 @@
+package chapter01.sector04
+
+/**
+ * val는 변수를 선언할 때 사용하는 키워드 (value의 줄임말)
+ * val은 내용을 다시 대입할 수 없음
+ * val 식별자 = 값
+ */
+fun main() {
+    val whole = 11
+    val fractional = 1.4
+    val words = "Twas Brilling"
+
+    // whole = 14 // Error: val은 변경할 수 없음
+
+    println(whole)
+    println(fractional)
+    println(words)
+}
---
+++ src/main/kotlin/chapter01/sector04/Vars.kt
@@ -0,0 +1,19 @@
+package chapter01.sector04
+
+/**
+ * var는 변수를 선언할 때 사용하는 키워드 (variable의 줄임말)
+ * var는 내용을 다시 대입할 수 있음
+ * var 식별자 = 값
+ */
+
+fun main() {
+    var whole = 11
+    var fractional = 1.4
+    var words = "Twas Brilling"
+
+    fractional += 3
+
+    println(whole)
+    println(fractional)
+    println(words)
+}
