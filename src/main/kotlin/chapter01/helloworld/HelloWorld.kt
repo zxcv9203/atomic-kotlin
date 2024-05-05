84f733c014f7d03cb399ae6549138ff0ac3519d0
yongckim <zxcv9203@naver.com>
2024-05-05 17:07:36 +0900

    :sparkles: hello world

---
+++ src/main/kotlin/chapter01/helloworld/HelloWorld.kt
@@ -0,0 +1,17 @@
+package chapter01.helloworld
+
+/**
+ * fun은 함수를 선언할 때 사용하는 키워드 (function의 줄임말)
+ * 함수 이름 뒤에는 파라미터 목록이 존재
+ * 파라미터 목록 다음에는 함수 본문이 위치 함수 본문은 여는 중괄호로 시작하고 닫는 중괄호로 끝남
+ * 그 안에는 문(statement) 식(expression)이 들어감
+ */
+fun main() {
+    // 주석
+    /**
+     * 여러 줄 주석
+     */
+
+    // 콘솔에 메시지 출력
+    println("Hello, World!")
+}
