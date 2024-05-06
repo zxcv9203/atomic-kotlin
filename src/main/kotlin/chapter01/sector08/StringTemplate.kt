c3b9977e540d9e594ba7d832414239c0b43a84c9
yongckim <zxcv9203@naver.com>
2024-05-06 15:07:46 +0900

    :sparkles: 문자열 템플릿 정리

---
+++ src/main/kotlin/chapter01/sector08/StringTemplate.kt
@@ -0,0 +1,23 @@
+package chapter01.sector08
+
+fun main() {
+    val answer = 42
+    // $를 사용하여 변수를 문자열에 포함시킬 수 있습니다.
+    println("The answer is $answer")
+    // $ 다음에 오는 대상이 프로그램 식별자로 인식되지 않으면 아무일도 일어나지 않습니다.
+    println("print $1")
+
+    // 문자열 연결을 통해 값을 넣을 수도 있습니다.
+    println("The answer is " + answer)
+
+    // $의 안에 식을 넣으면 그 식을 평가합니다.
+    val condition = true
+    println(if (condition) "right" else "wrong")
+    println("The answer is ${answer + 1}")
+
+    // String 안에 큰따옴표 등의 특수문자를 넣는 경우 역슬래시를 이용해 이스케이프 처리를 하거나
+    // 큰 따옴표를 연달아 세 개 쓰는 String 리터럴을 사용해야 함
+    val s = "value"
+    println("The value is \"$s\"")
+    println("""The value is "$s"""")
+}
