13e8706140736c740c96f36e06c52d13fdcdfed4
yongckim <zxcv9203@naver.com>
2024-05-06 15:23:34 +0900

    :sparkles: 불리언 정리

---
+++ src/main/kotlin/chapter01/sector09/Boolean.kt
@@ -0,0 +1,15 @@
+package chapter01.sector09
+
+/**
+ * && : 연산자 오른쪽과 왼쪽에 있는 Boolean 식이 모두 true일때만 true를 반환
+ * || : 연산자 오른쪽과 왼쪽에 있는 Boolean 식 중 하나라도 true이면 true를 반환
+ */
+fun main() {
+    println(true && true) // true
+    println(true && false) // false
+    println(false && false) // false
+
+    println(true || true) // true
+    println(true || false) // true
+    println(false || false) // false
+}
